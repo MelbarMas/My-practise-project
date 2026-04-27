c9130a3b8e92e983819c04860408dd01b24c2e80
MelbarMas <melbarmasora@gmail.com>
2026-04-27 21:36:27 +0100

    loops practise

---
+++ practise-day-8.py
@@ -0,0 +1,16 @@
+weekdays=['Monday', 'Tuesday','Wednesday', 'Thursday','Friday']
+
+#print(len('melbar'))
+for i in range(0, len(weekdays)):
+    print('i is' , i)
+    print(weekdays[i])
+
+
+
+"""
+pos=0
+while pos<5:
+    print(weekdays[0])
+    pos=pos+1
+"""
+
